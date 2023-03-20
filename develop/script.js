286a01b0e69841176cce3b7c23a3064c4df99f88
Amadeus <a.turner3x@gmail.com>
2023-03-20 12:49:15 -0400

    get weather with api , set default values

--- develop/script.js
+++ develop/script.js
@@ -90,3 +90,98 @@ let addList = () => {
   }
 };
 
+// Today
+
+todayDate.textContent = moment()
+  .format("dddd, MMMM Do, h:mm a");
+
+// getWeather (API) = showWeather
+let showWeather = (weather, searchQuery) => {
+  cityEl.textContent = searchQuery;
+  iconEl = weather.weather[0].icon;
+  document.getElementById("todayIcon")
+    .src = "./assets/css/icons/" + iconEl + ".png"; // add icon to assets folder
+  document.getElementById("todayTemp")
+    .innerHTML = weather.main.temp;
+  document.getElementById("todayHumidity")
+    .innerHTML = weather.main.humidity;
+  document.getElementById("todayWind")
+    .innerHTML = weather.wind.speed;
+};
+
+// 5 day forecast
+
+document.getElementById("day1")
+  .innerHTML = moment()
+  .add(1, "d")
+  .format("MMMM Do");
+document.getElementById("day2")
+  .innerHTML = moment()
+  .add(2, "d")
+  .format("MMMM Do");
+document.getElementById("day3")
+  .innerHTML = moment()
+  .add(3, "d")
+  .format("MMMM Do");
+document.getElementById("day4")
+  .innerHTML = moment()
+  .add(4, "d")
+  .format("MMMM Do");
+document.getElementById("day5")
+  .innerHTML = moment()
+  .add(5, "d")
+  .format("MMMM Do");
+
+let showForecast = (forecast, searchQuery) => {
+  cityEl.textContent = searchQuery;
+  // 1/5
+  document.getElementById("t1")
+    .innerHTML = forecast.list[4].main.temp;
+  document.getElementById("h1")
+    .innerHTML = forecast.list[4].main.humidity;
+  iconEl1 = forecast.list[4].weather[0].icon;
+  document.getElementById("i1")
+    .src = "./assets/css/icons/" + iconEl1 + ".png";
+  // 2/5
+  document.getElementById("t2")
+    .innerHTML = forecast.list[12].main.temp;
+  document.getElementById("h2")
+    .innerHTML = forecast.list[12].main.humidity;
+  iconEl2 = forecast.list[12].weather[0].icon;
+  document.getElementById("i2")
+    .src = "./assets/css/icons/"  + iconEl2 + ".png";
+  // 3/5
+  document.getElementById("t3")
+    .innerHTML = forecast.list[20].main.temp;
+  document.getElementById("h3")
+    .innerHTML = forecast.list[20].main.humidity;
+  iconEl3 = forecast.list[20].weather[0].icon;
+  document.getElementById("i3")
+    .src = "./assets/css/icons/" + iconEl3 + ".png";
+  // 4/5
+  document.getElementById("t4")
+    .innerHTML = forecast.list[28].main.temp;
+  document.getElementById("h4")
+    .innerHTML = forecast.list[28].main.humidity;
+  iconEl4 = forecast.list[28].weather[0].icon;
+  document.getElementById("i4") 
+    .src = "./assets/css/icons/" + iconEl4 + ".png";
+  // 5/5
+  document.getElementById("t5")
+    .innerHTML = forecast.list[36].main.temp;
+  document.getElementById("h5")
+    .innerHTML = forecast.list[36].main.humidity;
+  iconEl5 = forecast.list[36].weather[0].icon;
+  document.getElementById("i5")
+    .src = "./assets/css/icons/" + iconEl5 + ".png";
+  // end 5 day Forecast
+};
+
+//listeing for submission
+cityForm.addEventListener("submit", submitQuery);
+listCity();
+addList();
+
+// OPEN WITH DEFAULT VALUES
+getWeather("Detroit");
+getForecast("Detroit");
